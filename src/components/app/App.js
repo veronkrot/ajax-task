824cc6e8202fa9703ec6ec77c89be3fb7ac85fd9
veronkrot <veronkrotenko9@gmail.com>
2021-01-19 20:06:03 +0200

    fixed gitignore

---
+++ src/components/app/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from "react";
+import {PeopleComp} from "./Components/PeopleComp";
+
+const App = () => {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <PeopleComp/>
+            </header>
+        </div>
+    );
+}
+
+export default App;
